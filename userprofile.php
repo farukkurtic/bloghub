44578ec07f00026c5fd19cd8176ffce0813b01a1
Faruk Kurtić <34779712+farukkurtic@users.noreply.github.com>
2025-07-31 05:25:09 +0200

    Update userprofile.php

--- userprofile.php
+++ userprofile.php
@@ -15,13 +15,10 @@ $user_id = $_GET["user_id"];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <!-- Google fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
-    <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/546f520f0b.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./styles/style.css">
     <title>Profile</title>
@@ -74,24 +71,18 @@ $user_id = $_GET["user_id"];
                         echo "</div>";
                     }
                 } else {
-                    // Prikazivanje teksta i animirane ikone ako objave ne postoje
                     echo "<div class='empty'>";
                     echo '<i class="fa-solid fa-bookmark fa-bounce fa-10x" style="color: #0099ff;"></i>';
                     echo '<h2 class="no-posts">No posts found</h2>';
                     echo "</div>";
                 }
 
-                // Free the result set
                 mysqli_free_result($post_result);
                 mysqli_free_result($user_result);
             
             ?>
 
         </div>
-        <!-- footer -->
-        <footer>
-            <p>&copy; 2023 Faruk Kurtić. All rights reserved.</p>
-        </footer>
     </div>
 </body>
 </html>
