94241e660b943750e5b97cb89f86147437ec63f4
Faruk Kurtić <34779712+farukkurtic@users.noreply.github.com>
2025-07-31 05:19:41 +0200

    Update blog.php

--- blog.php
+++ blog.php
@@ -4,7 +4,6 @@
 
 session_start();
 
-// brisanje objave
 if(isset($_POST["delete_submit"])) {
     $post_id = $_POST["delete_submit"];
     $delete_query = "DELETE FROM posts WHERE post_id = ?";
@@ -15,7 +14,6 @@ if(isset($_POST["delete_submit"])) {
     header("Location: profile.php");
 }
 
-// editovanje objave
 if(isset($_POST["edit_submit"])) {
     
     $post_id = $_POST["edit_submit"];
@@ -32,13 +30,10 @@ if(isset($_POST["edit_submit"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <!-- Google fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
-    <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/546f520f0b.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./styles/style.css">
     <title>Profile</title>
@@ -82,8 +77,8 @@ if(isset($_POST["edit_submit"])) {
 
                 echo "<div class='post'>";
                 echo "<h2>" . $post['title'] . "</h2>";
-                echo "<p>" . substr($post['post'], 0, 200) . "...</p>"; // Display a truncated version of the post content
-                echo "<a href='fullpost.php?post_id={$post['post_id']}'>Read more</a>"; // Link to the full post
+                echo "<p>" . substr($post['post'], 0, 200) . "...</p>"; 
+                echo "<a href='fullpost.php?post_id={$post['post_id']}'>Read more</a>";
                 echo "<p>" . "Posted by " . "<b>" . "<a href='userprofile.php?user_id=" . $postUserId . "'>" . $user["full_name"] . "</a>" . "</b>" . " on " . $post["date_created"] . "</p>";
                 echo "<hr>";
                 echo "</div>";
@@ -92,7 +87,6 @@ if(isset($_POST["edit_submit"])) {
             ?> 
 
         </div>
-        <!-- footer -->
         <footer>
             <p>&copy; 2023 Faruk Kurtić. All rights reserved.</p>
         </footer>
